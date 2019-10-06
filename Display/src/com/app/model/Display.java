8a67290e27269707cb029d37357eab1f67879d3f
shubham1 <java.shubham1995@gmail.com>
2019-10-06 12:15:00 -0700

    adding comment sopln stmt

--- Display/src/com/app/model/Display.java
+++ Display/src/com/app/model/Display.java
@@ -6,8 +6,9 @@ package com.app.model;
  */
 
 public class Display {
+	
 	public static void main(String[] args) {
-		
+		//adding sopln stmt
 		System.out.println("welcome to github world..!");
 		System.out.println("welcome to world......!!!");
 		System.out.println("hello world");
